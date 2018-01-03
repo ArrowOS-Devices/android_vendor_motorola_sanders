048b6e4593a8a1ab3e6d7d5cfff8a028e7ef6110
Vachounet <vachounet@live.fr>
2018-01-03 14:17:42 +0100

    sanders: update qti-telephony-common to oreo

--- sanders-vendor.mk
+++ sanders-vendor.mk
@@ -1290,7 +1290,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     TimeService \
-    datastatusnotification \
     embms \
     colorservice \
     com.qti.location.sdk \
@@ -1305,6 +1304,7 @@ PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     izat.xt.srv \
     qti-telephony-common \
+    QtiTelephonyServicelibrary \
     atfwd \
     cneapiclient \
     com.qti.dpmframework \
